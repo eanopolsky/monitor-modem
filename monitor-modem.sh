6df14fbf284c4bf9e72ec5ebd8bc821bedf40078
Eric Anopolsky <erpo41@gmail.com>
2018-09-05 20:27:50 -0600

    add remote control via signals

--- monitor-modem.sh
+++ monitor-modem.sh
@@ -6,7 +6,6 @@ print_syslog() {
     logger --id=$$ -t monitor-modem "$1"
 }
 
-
 test_connectivity() {
     # Returns 0 if *any* host responds to ping.
     # If no hosts respond to ping, returns 1.
@@ -38,16 +37,42 @@ init_gpio() {
     echo "out" > "/sys/class/gpio/gpio${RELAYPIN}/direction"
 }
 
-reset_modem() {
-    # Cut power to the modem for ten seconds.
-    print_syslog "Power cycling modem for 10 seconds."
+# monitor-modem can be placed into one of two modes by sending it signals. In
+# the default mode, keep_link_up, monitor-modem checks for network connectivity
+# and power cycles the modem if the network has been out for too long. In the
+# alternate mode, keep_modem_off, it powers down the modem until another signal
+# is received setting it back to the default mode. This is designed to allow
+# Internet access to be scheduled with cron.
+init_signal_control() {
+    OPERATIONMODE="keep_link_up"
+    trap OPERATIONMODE="keep_modem_off" SIGUSR1
+    trap OPERATIONMODE="keep_link_up" SIGUSR2
+}
+
+init() {
+    init_gpio
+    init_signal_control
+}
+
+power_off_modem() {
+    print_syslog "Powering off modem."
     echo "1" > "/sys/class/gpio/gpio${RELAYPIN}/value"
-    sleep 10
+}
+
+power_on_modem() {
+    print_syslog "Powering on modem."
     echo "0" > "/sys/class/gpio/gpio${RELAYPIN}/value"
 }
 
+reset_modem() {
+    # Cut power to the modem for ten seconds.
+    print_syslog "Power cycling modem for 10 seconds."
+    power_off_modem
+    sleep 10
+    power_on_modem
+}
 
-init_gpio
+init
 
 # Sometimes it takes a few seconds for DHCP to get an IP and bring up the
 # network interface. Running ping before that happens results in a very quick
@@ -56,23 +81,36 @@ init_gpio
 sleep 30
 
 while true
-do test_connectivity
-   if [ "$?" = "0" ]
-   then sleep 300
-	continue
-   else
-       # Sometimes the connectivity check will fail, but the modem will recover
-       # on its own within seconds or minutes. Because it can take up to 10
-       # minutes for the modem to recover from a hard reboot, we give it one
-       # more chance to come back on its own first.
-       sleep 300
-       test_connectivity
-       if [ "$?" = "0" ]
-       then sleep 300
-	    continue
-       else
-	   reset_modem
-	   sleep 1800
-       fi
+do if [ "$OPERATIONMODE" = "keep_link_up" ]
+   then test_connectivity
+	if [ "$?" = "0" ]
+	then sleep 300
+	     continue
+	else
+	    # Sometimes the connectivity check will fail, but the modem
+	    # will recover on its own within seconds or minutes. Because
+	    # it can take up to 10 minutes for the modem to recover from
+	    # a hard reboot, we give it one more chance to come back on
+	    # its own first.
+	    sleep 300
+	    test_connectivity
+	    if [ "$?" = "0" ]
+	    then sleep 300
+		 continue
+	    else
+		reset_modem
+		sleep 1800
+	    fi
+	fi
+   elif [ "$OPERATIONMODE" = "keep_modem_off" ]
+   then power_off_modem
+	while [ "$OPERATIONMODE" = "keep_modem_off" ]
+	do sleep 60
+	done
+	power_on_modem
+	# Give the modem time to reestablish a connection before considering
+	# power cycling it in keep_link_up mode:
+	sleep 1800 
+   else print_syslog "Unsupported operation mode."; exit 1
    fi
 done
